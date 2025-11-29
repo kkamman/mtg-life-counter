135833c0a12db92e8ea27d9012e0ef989fe9ae47
Koen Kamman <koen@kamman.dev>
2025-11-29 14:15:09 +0100

    feat: add fullscreen toggle

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,18 +1,20 @@
 import { Component, effect, inject, signal } from '@angular/core';
-import { Field, form } from '@angular/forms/signals';
+import { disabled, Field, form } from '@angular/forms/signals';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { ThemeStore } from '../core/theme-store';
 
 interface SettingsData {
   theme: 'system' | 'light' | 'dark';
+  isFullscreen: boolean;
 }
 
 @Component({
   selector: 'app-settings',
-  imports: [MatRadioGroup, MatRadioButton, Field],
+  imports: [MatRadioGroup, MatRadioButton, Field, MatSlideToggle],
   templateUrl: './settings.html',
   host: {
-    class: 'block p-4',
+    class: 'flex flex-col p-4 gap-4',
   },
 })
 export class Settings {
@@ -20,11 +22,36 @@ export class Settings {
 
   private readonly settingsModel = signal<SettingsData>({
     theme: this.themeStore.theme(),
+    isFullscreen: document.fullscreenElement !== null,
   });
 
-  protected readonly settingsForm = form(this.settingsModel);
+  protected readonly settingsForm = form(this.settingsModel, (settings) => {
+    if (!document.fullscreenEnabled) {
+      disabled(settings.isFullscreen);
+    }
+  });
 
   constructor() {
     effect(() => this.themeStore.theme.set(this.settingsForm.theme().value()));
+    this.updateIsFullscreenOnChange();
+  }
+
+  protected toggleFullscreen() {
+    const isFullscreenRequested = this.settingsForm.isFullscreen().value();
+
+    if (isFullscreenRequested !== this.isFullscreenOpen()) {
+      isFullscreenRequested ? document.body.requestFullscreen() : document.exitFullscreen();
+    }
+  }
+
+  private updateIsFullscreenOnChange() {
+    const updateFn = () => this.settingsForm.isFullscreen().value.set(this.isFullscreenOpen());
+
+    document.body.addEventListener('fullscreenchange', updateFn);
+    document.body.addEventListener('fullscreenerror', updateFn);
+  }
+
+  private isFullscreenOpen() {
+    return document.fullscreenElement !== null;
   }
 }
