be5ded2d9addaaf918f097d11a9ebdd3bb1f9c7e
Koen Kamman <koen@kamman.dev>
2025-12-05 20:42:20 +0100

    feat: add life change counter

--- src/app/player-card/player-card.ts
+++ src/app/player-card/player-card.ts
@@ -1,7 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { MatIconButton } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
+import {
+  concat,
+  delay,
+  map,
+  of,
+  pairwise,
+  repeat,
+  scan,
+  startWith,
+  switchMap,
+  takeWhile,
+} from 'rxjs';
 
 @Component({
   selector: 'app-player-card',
@@ -16,7 +29,19 @@ export class PlayerCard {
   protected readonly maxLife = 999;
   protected readonly minLife = 0;
 
-  public readonly life = signal(this.startingLife);
+  protected readonly life = signal(this.startingLife);
+
+  protected readonly lifeChange = toSignal(
+    toObservable(this.life).pipe(
+      pairwise(),
+      scan((acc, curr) => acc + (curr[1] - curr[0]), 0),
+      switchMap((x) => concat(of(x), of(0, null).pipe(delay(3000)))),
+      takeWhile((x) => x != null),
+      map((x) => (x > 0 ? `+${x}` : x)),
+      repeat(),
+      startWith(0)
+    )
+  );
 
   protected removeLife() {
     this.life.update((count) => Math.max(count - 1, this.minLife));
